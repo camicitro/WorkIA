f17a5f839bf1709026e21f757af3cb1fa8fe8e7e
camicitro <camilacitro1106@gmail.com>
2026-01-15 11:54:22 -0300

    a

---
+++ src/engine/matching.py
@@ -0,0 +1,7 @@
+"""
+el motor debe leer los multiplicadores directamente desde el nodo Oferta que creamos en Neo4j
+
+La fórmula que implementaremos para el $score\_final$ será:$$score\_final = (S_{tec} \cdot W_{tec}) + (S_{blan} \cdot W_{blan}) + (S_{exp} \cdot W_{exp})$$
+
+
+"""
